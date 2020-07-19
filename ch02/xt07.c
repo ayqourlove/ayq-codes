8213d8ed7c3311a736a181154e146ee098057cc0
ayqourlove <2011013247@qq.com>
2020-07-19 22:53:46 +0800

    ch02: xt07

---
+++ ch02/xt07.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void smile(void)
+{
+    printf ("smile!");
+
+}
+int main()
+{
+    smile(),smile(),smile();
+    printf ("\n");
+    smile(),smile();
+    printf ("\n");
+    smile();
+    return 0;
+}
